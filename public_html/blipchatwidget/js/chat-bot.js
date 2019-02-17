898d329b02b6c3a058acaf5a864caaf6ed0e8b4a
Manasses Java <manasses.java@gmail.com>
2019-02-17 19:15:34 -0300

    100 COMENTARIOS

---
+++ public_html/blipchatwidget/js/chat-bot.js
@@ -0,0 +1,8 @@
+(function () {
+    window.onload = function () {
+        new BlipChat()
+                .withAppKey('bWFuYWg2OjE5M2JjODkyLWNjYjktNDRmNy05YjAxLTM4ODFiYzc1NTAwMw==')
+                .withButton({"color": "#00ff40", "icon": "img/dollynho-seu-amiguinho.png"})
+                .build();
+    }
+})();                                
